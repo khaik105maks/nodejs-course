6cfff9d88926e5cd6f957c3292e12985e3fc24e4
Maks Bychok <test@khai.edu>
2023-10-03 12:57:37 +0300

    Add implementation

--- src/login/login.js
+++ src/login/login.js
@@ -1,1 +1,6 @@
-console.log('login component')
+console.log('login component');
+console.log('login component');
+console.log('login component');
+console.log('login component');
+console.log('login component');
+console.log('login component');
