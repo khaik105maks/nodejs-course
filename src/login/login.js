cba9d765407dcddbdc4d313cf21050fed1b84abd
Maks Bychok <test@khai.edu>
2023-10-18 23:20:42 +0300

    fix

--- src/login/login.js
+++
@@ -1,6 +0,0 @@
-console.log('login component');
-console.log('login component');
-console.log('login component');
-console.log('login component');
-console.log('login component');
-console.log('login component');
