1a11c1fab5c01198f184c78b67949d9790c34d8e
Volodumur218a <Volodumur3310@gmail.com>
2025-12-13 21:20:25 +0200

    add express router, middleware, error handling

--- index.js
+++ index.js
@@ -2,11 +2,22 @@
 const express = require('express');
 const app = express();
 
+const logger = require('./middleware');
+const productsRouter = require('./products');
+const errorHandler = require('./error-middleware');
+
 const port = 3000;
 
+app.use(logger);
+
+app.use('/products', productsRouter);
+
 app.get('/', (req, res) => {
-  res.send('response for GET request');
+  res.send('Express app with modules, middleware, error handling');
 });
+
+// має бути останнім
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`server start at http://localhost:${port}/`);
