ab69f301c626f809be9ad9620c169cc6d03f6d44
Ra13fEwsB1 <y.o.pleshkan@student.khai.edu>
2026-06-03 06:09:15 +0200

    lab4 add user products endpoint

--- src/product/product.router.js
+++ src/product/product.router.js
@@ -1,18 +1,32 @@
 const express = require('express');
+const { eq } = require('drizzle-orm');
 const db = require('../db');
 const { products } = require('../db/schema');
 
 const router = express.Router();
 
 router.post('/products', async (request, response) => {
-    const { body } = request;
-    await db.insert(products).values(body);
-    return response.sendStatus(201);
+  const { body } = request;
+
+  await db.insert(products).values(body);
+
+  return response.sendStatus(201);
 });
 
 router.get('/products', async (request, response) => {
-    const products = await db.query.products.findMany();
-    return response.json(products);
+  const products = await db.query.products.findMany();
+
+  return response.json(products);
+});
+
+router.get('/users/:id/products', async (request, response) => {
+  const { id } = request.params;
+
+  const userProducts = await db.query.products.findMany({
+    where: eq(products.userId, +id)
+  });
+
+  return response.json(userProducts);
 });
 
 module.exports = router;
